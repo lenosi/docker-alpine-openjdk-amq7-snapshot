a781391eb55a47fd547c42ceaccd415cfe6e49b1
Jiri Danek <jdanek@redhat.com>
2017-10-10 11:04:15 +0200

    Switch from AMQ 7.0.0 SNAPSHOT to Artemis 2.4.0-SNAPSHOT

---
+++ setup_broker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# Set sources
+PREFIX='apache-artemis'
+RELEASE='2.4.0'
+REPO="https://repository.apache.org/content/repositories/snapshots/org/apache/activemq/apache-artemis/${RELEASE}-SNAPSHOT/"
+VERSION=$(wget -O - -o /dev/null $REPO/maven-metadata.xml | grep -oP '(?<=<value>).*?(?=</value>)' | head -1)
+FILENAME="${PREFIX}-${VERSION}-bin.zip"
+mkdir /opt && cd /opt
+wget -q $REPO/${FILENAME}
+wget -q $REPO/${FILENAME}.sha1
+# Verify
+sha1sum ${FILENAME} | grep $(cat ${FILENAME}.sha1)
+# Unpack and create link
+unzip ${FILENAME}
+ln -s ${PREFIX}-${RELEASE}-SNAPSHOT A-MQ7
+rm -rf ${FILENAME}
+# Verify package @TODO
